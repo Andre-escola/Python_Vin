c1439527eed5bf4bd8f8ab783479832c66c114a5
Andre-escola <132575305+Andre-escola@users.noreply.github.com>
2023-09-15 12:09:45 -0300

    Add files via upload

---
+++ Python-André/teste.py
@@ -0,0 +1,10 @@
+#Variáveis
+#print ('hello gays')
+#idade = 18
+#print(idade)
+#velocidade_maxima = 240
+#print(velocidade_maxima)
+#Frase_De_Boas_Vindas = "olá, seja bem vindo" #string
+#print(Frase_De_Boas_Vindas)
+Posso_Entrar_Em_Contato = False #Boleanos
+print(Posso_Entrar_Em_Contato)
